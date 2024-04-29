5b23aa1a8bdf3295bad013b83e1642e3806cc6ea
Pxluthor <34409005+pxluthor@users.noreply.github.com>
2024-04-29 07:19:19 -0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -31,7 +31,7 @@ audio_file = st.file_uploader("Upload Audio ou video", type=["wav","mp3","ogg","
 if audio_file is not None:
     # Importing model -- base(74M parameter)
     model = whisper.load_model("base")
-    st.info("Whisper model loaded")
+    st.info("Whisper model loaded") 
 
     # Playing audio file
     st.header("Play áudio:")
